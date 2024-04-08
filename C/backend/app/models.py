2c590f35b0e402935ac353761d3f6833a6088fba
Alan Joseph <notalanjoseph@gmail.com>
2024-04-08 22:13:10 +0530

    delete unwanted

--- C/backend/app/models.py
+++ C/backend/app/models.py
@@ -7,28 +7,6 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
-'''
-class User(Base):
-    __tablename__ = "institutions"
-
-    name = Column(String, nullable=False, unique=True) # has to be unique to avoid confusion for students
-    email = Column(String, primary_key=True, nullable=False)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
-    otp = Column(Integer, nullable=True)
-
-class Student(Base):
-    __tablename__ = "students"
-
-    institution = Column(String, ForeignKey('institutions.name'), nullable=False)
-    id = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    exams_attended = Column(ARRAY(String), nullable=True)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
-
-    __table_args__ = (PrimaryKeyConstraint('institution', 'id'),)
-'''
-
 class Exam(Base):
     __tablename__ = "exams"
 
--- C/backend/ocr.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-reader = easyocr.Reader(['en'])
-result = reader.readtext('level2.png')
-
-for (bbox, text, prob) in result:
-    print(text)
